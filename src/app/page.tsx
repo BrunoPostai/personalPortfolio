b4977f833f138e38df88f74a4790839e9189138d
BrunoPostai <cechinelbruno@gmail.com>
2023-11-27 17:49:28 +0100

    feature(header)added header section of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { Icon } from '@iconify/react';
 import Image from "next/image";
 import  Header  from "../components/header/";
 export default function Home() {
   return (
     <div>
-      <h1>main page</h1>
       <Header/>
     </div>
   );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,27 @@
 "use client";
-import { Button } from '@mui/material';
-import React from 'react';
-import { Icon } from '@iconify/react';
-
-// import { Container } from './styles';
+import { Button } from "@mui/material";
+import React from "react";
+import { Icon } from "@iconify/react";
+import "./styles.header.css";
 
 const Header: React.FC = () => {
-  return <Icon icon="mdi-light:home" />;
-}
+  return (
+    <div className="wrapper">
+      <header>
+        <a href="#" className="logo">BP</a>
+        <nav>
+          <ul>
+            <li><a href="#">PROJECTS</a></li>
+            <li><a href="#">ABOUT ME</a></li>
+            <li id="contato"><button><a href="#">CONTACT</a></button></li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <p>I AM<br/><span id="name">BRUNO POSTAI</span><br/> FRONT-END DEVELOPER</p>
+      </section>
+    </div>
+  );
+};
 
 export default Header;
