f81533a7f77097b86e7fd26403d48a57c859a00a
BrunoPostai <cechinelbruno@gmail.com>
2023-11-27 14:52:05 +0100

    feature(global.css)configured standart css, also  added MUI, ICONIFY and AXIOS

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+// import { Container } from './styles';
+
+const pages: React.FC = () => {
+  return <Button variant="contained">Hello world</Button>;
+}
+
+export default pages;
---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from '@mui/material';
+import React from 'react';
+import { Icon } from '@iconify/react';
+
+// import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return <Icon icon="mdi-light:home" />;
+}
+
+export default Header;
