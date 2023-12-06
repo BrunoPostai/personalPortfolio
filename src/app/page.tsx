8b2efde37ab6ff9ed1dd5a4ef88f0d2a46635639
BrunoPostai <cechinelbruno@gmail.com>
2023-12-06 14:20:05 +0100

    feature(PWA)added a PWA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 import Image from "next/image";
-import  Header  from "../components/header/";
-import  About  from "../components/aboutMe/";
-import  Projects  from "../components/myProjects/";
-import  Contact  from "../components/contacts/";
+import Header from "../components/header/";
+import About from "../components/aboutMe/";
+import Projects from "../components/myProjects/";
+import Contact from "../components/contacts/";
+import manifest from "../../public/manifest.json";
+
+if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./pwabuilder-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch((error) => {
+      console.error('Erro ao registrar Service Worker:', error);
+    });
+}
+
 export default function Home() {
   return (
     <div>
-      <Header/>
-      <About/>
-      <Projects/>
-      <Contact/>
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
