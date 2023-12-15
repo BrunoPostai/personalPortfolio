ed53568520ca8ba149189836d4e163795a6f10d5
BrunoPostai <cechinelbruno@gmail.com>
2023-12-15 11:17:39 +0100

    chore(css)improving the responsability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
+  themeColor: '#1F1F1F',
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,6 @@ import Header from "../components/header/";
 import About from "../components/aboutMe/";
 import Projects from "../components/myProjects/";
 import Contact from "../components/contacts/";
-import manifest from "../../public/manifest.json";
-
-if (typeof navigator !== 'undefined' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./pwabuilder-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registrado com sucesso:', registration);
-    })
-    .catch((error) => {
-      console.error('Erro ao registrar Service Worker:', error);
-    });
-}
 
 export default function Home() {
   return (
