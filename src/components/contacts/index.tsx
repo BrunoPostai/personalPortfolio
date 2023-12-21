c21fa40a68afc5d128a7be0c9e565f9cfbabf1bf
BrunoPostai <cechinelbruno@gmail.com>
2023-12-21 09:22:16 +0100

    chore(contacts)adding my social media on icon links

--- src/components/contacts/index.tsx
+++ src/components/contacts/index.tsx
@@ -9,10 +9,10 @@ function Contact() {
         <div className={styles.imageBox}>
         </div>
         <div className={styles.socialMedia}>
-          <Icon icon="ri:instagram-fill" color="#3a86df" />
-          <Icon icon="ic:baseline-facebook" color="#3a86df" />
-          <Icon icon="mdi:linkedin" color="#3a86df" />
-          <Icon icon="mingcute:pdf-fill" color="#3a86df" />
+         <a className={styles.link} href="https://www.instagram.com/bpostai/" target="_blank"><Icon icon="ri:instagram-fill" color="#3a86df" /></a>
+          <a className={styles.link} href="https://www.facebook.com/brunogabriel.postaicardoso" target="_blank"><Icon icon="ic:baseline-facebook" color="#3a86df" /></a>
+          <a className={styles.link} href="https://www.linkedin.com/in/bruno-postai/" target="_blank"><Icon icon="mdi:linkedin" color="#3a86df" /></a>
+          <a className={styles.link} href="https://drive.google.com/file/d/1YHYOL3up1XtqQaj2iG70QzEa5owgp2RH/view?usp=sharing" target="_blank"><Icon icon="mingcute:pdf-fill" color="#3a86df"/></a>
         </div>
       </div>
       <div className={styles.leftWrap}>
