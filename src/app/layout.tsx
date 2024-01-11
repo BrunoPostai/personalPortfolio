fff10cf13c1380581b7e644a770f309cc50a9eee
BrunoPostai <cechinelbruno@gmail.com>
2024-01-11 10:19:34 +0100

    chore(general)trying to fix some stuff to improve speedInsight scores

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,9 +47,10 @@ export const metadata: Metadata = {
   },
 };
 export const viewport: Viewport = {
+  userScalable: true,
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+  maximumScale: 5,
   themeColor: "#3576CB",
 };
 
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'personal-portfolio-delta-murex.vercel.app' + 'sitemap.xml',
+    sitemap: 'personal-portfolio-delta-murex.vercel.app',
   }
 }
